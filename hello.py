18ef4f04162633b4fb9d69678a20471303620dd6
jji@citrushack2016 <jdji@uci.edu>
2016-11-13 04:53:23 -0800

    arstarst

--- hello.py
+++ hello.py
@@ -24,6 +24,7 @@ def eventpage():
 def callback():
     #if getting auth code...
     auth_code = request.args.get("code")
+    print("THE AUTH_CODE IS: " + auth_code)
     if request.args.get("code"):
         payload = {"client_id": APPLICATION_ID, "client_secret": APP_SECRET, "code": auth_code}
         response = requests.post(post_for_token, data=payload)
