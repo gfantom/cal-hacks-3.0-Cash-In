d5cfa12b624a8d09fd8a460d4f55166eb634f0aa
jji@citrushack2016 <jdji@uci.edu>
2016-11-12 17:45:59 -0800

    updating backend
    yeup

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
 import requests
+from config import *
+
+auth_code = ""
+access_token = ""
+post_for_token = "https://connect.squareup.com/oauth2/token"
 
 app = Flask(__name__)
 
@@ -13,7 +18,15 @@ def login():
 
 @app.route("/callback")
 def callback():
+    auth_code = requests.args.get("code")
+    if auth_code != None:
+        payload = {"client_id": APPLICATION_ID, "client_secret":
+                   APP_SECRET, "code": auth_code}
+        response = requests.post(post_for_token, data=payload)
+        access_token = response.headers["access_token"]
+    return render_template("partySetup.html")
     
 
 if __name__ == "__main__":
     app.run()
+    
