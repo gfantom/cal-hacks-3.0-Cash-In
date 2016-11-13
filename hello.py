6bb730570667bca925bd74c3b43b3665115e879b
jji@citrushack2016 <jdji@uci.edu>
2016-11-13 06:11:18 -0800

    adding stuff to flask server

--- hello.py
+++ hello.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import requests
 from config import *
+import urllib
 
 auth_code = ""
 access_token = ""
 post_for_token = "https://connect.squareup.com/oauth2/token"
+fee = 0
+event_label = ""
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return redirect(url_for("login"))
 
 @app.route("/login")
 def login():
@@ -20,10 +23,35 @@ def login():
 def eventpage():
     return render_template("eventpage.html")
 
+@app.route("/eventsetup")
+def eventsetup():
+    return render_template("eventsetup.html")
+
+@app.route("/finishsetup")
+def finishsetup():
+    fee = float(request.args.get("fee"))
+    event_label = request.args.get("label")
+    return redirect(url_for("eventpage"))
+
+@app.route("/charge")
+def charge():
+    data = {"amount_money": {"amount": fee, "currency_code": "USD"},
+            "callback_url": "myapp-url-scheme://payment-complete",
+            "client_id": APPLICATION_ID,
+            "version": "1.1",
+            "notes": event_label,
+            "options": {"supported_tender_types": ["CREDIT_CARD","CASH"]},
+            "clear_default_fees": True,
+            "auto_return": True,
+            "skip_receipt": True}
+    return redirect("square-commerce-v1://payment/create?data=" +
+                    urllib.urlencode(data))
+    
+
 @app.route("/callback")
 def callback():
     #if getting auth code...
-    auth_code = request.args.get("code")
+    #auth_code = request.args.get("code")
     print("THE AUTH_CODE IS: " + auth_code)
     if request.args.get("code"):
         payload = {"client_id": APPLICATION_ID, "client_secret": APP_SECRET, "code": auth_code}
